523ef42a4d05ec03a55271a96e139968523333c8
Mayur.jangale <mayur.jangale2013@gmail.com>
2023-02-09 16:09:07 +0530

    productcatalog added

---
+++ src/test/java/pkg/productCatalogue.java
@@ -0,0 +1,17 @@
+package pkg;
+
+import org.testng.annotations.Test;
+
+import org.testng.annotations.Test;
+
+public class productCatalogue {
+
+	
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("****");
+		
+	}
+}
